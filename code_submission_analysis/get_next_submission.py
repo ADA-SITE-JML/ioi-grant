0a2220a16ac097ce0c2504e47a2c49083721bdf7
gadirlih <gadirlih@gmail.com>
2021-06-04 15:34:15 +0400

    add todo steps for get_next_submission.py

--- code_submission_analysis/get_next_submission.py
+++ code_submission_analysis/get_next_submission.py
@@ -8,3 +8,12 @@ cur = conn.cursor()
 cur.execute("SELECT * from submissions")
 
 rows = cur.fetchall()
+
+# 1.get 1 after the latest fetched submission
+# 2.check if it the user made submission has a prev submission on the same task
+# 3.if yes then fetch that submission and return their add in filesystem
+# 4.if no then return only that submission
+
+
+# 1.1 Store last fetched submission in a file
+# 1.2 add functionality to reset that value
